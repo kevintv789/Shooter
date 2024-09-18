536b379bcdfc859cf182d52882a30548ab3a6214
kevintv789 <kevintv789@gmail.com>
2024-09-17 21:58:26 -0700

    finished pathfinding and skeleton animation

--- scenes/enemies/hunter.gd
+++ scenes/enemies/hunter.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
 var active: bool = false
+var player_near: bool = false
 var speed: int = 200
+var monster_damage: int = 20
+var health: int = 100
 
 func _ready() -> void:
+	$NavigationAgent2D.path_desired_distance = 4.0
+	$NavigationAgent2D.target_desired_distance = 4.0
 	$NavigationAgent2D.target_position = Globals.player_position
 
 func _physics_process(_delta: float) -> void:
@@ -11,26 +16,35 @@ func _physics_process(_delta: float) -> void:
 	var direction: Vector2 = (next_path_pos - global_position).normalized()
 	velocity = direction * speed
 	move_and_slide()
-	look_at(next_path_pos)
-	if active:
-		look_at(Globals.player_position)
+	
+	var look_angle = direction.angle()
+	rotation = look_angle + PI / 2
 
 func _on_notice_area_body_entered(_body: Node2D) -> void:
 	active = true
-
+	$AnimationPlayer.play("walk")
 
 func _on_notice_area_body_exited(_body: Node2D) -> void:
 	active = false
-
+	$AnimationPlayer.stop()
 
 func _on_attack_area_body_entered(_body: Node2D) -> void:
-	pass # Replace with function body.
-
+	player_near = true
+	$AnimationPlayer.play("attack")
 
 func _on_attack_area_body_exited(_body: Node2D) -> void:
-	pass # Replace with function body.
-
+	player_near = false
+	$AnimationPlayer.play("walk")
 
 func _on_navigation_timer_timeout() -> void:
 	if active:
 		$NavigationAgent2D.target_position = Globals.player_position
+
+func attack() -> void:
+	if player_near:
+		Globals.health -= monster_damage
+
+func hit(damage: int) -> void:
+	health -= damage
+	if health <= 0:
+		queue_free()
